ad9a0a28110a78850c712117a312736a8cd65ed7
yuvaanparwaney <yuvaanparwaney@gmail.com>
2020-05-06 20:52:42 -0700

    angry birds done

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var bird, slingShot;
+var bird, ss;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -38,8 +38,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    ss = new SlingShot(bird.body,{x: 200, y:100});
 }
 
 function draw(){
@@ -63,6 +62,11 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    ss.display();  
+}
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x: mouseX, y:mouseY});
+}
+function mouseReleased(){
+    ss.fly();
 }
